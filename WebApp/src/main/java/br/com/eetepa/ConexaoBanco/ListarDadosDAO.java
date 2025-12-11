7a62fb2ffe96f618dab51d8c6fc3b5419bf0a03c
Joao-Vitor-2008 <joaovitornascimentolopes4@gmail.com>
2025-12-11 01:12:19 -0300

    Modifiquei o DAO para listar o bd para usar só uma conexão

--- WebApp/src/main/java/br/com/eetepa/ConexaoBanco/ListarDadosDAO.java
+++ WebApp/src/main/java/br/com/eetepa/ConexaoBanco/ListarDadosDAO.java
@@ -24,44 +24,46 @@ public class ListarDadosDAO {
     String sqlEstacoes = "SELECT data_hora, nome, id FROM estacoes;";
     String sqlIrrigadores = "SELECT id, id_estacao, plantio, acao_atual FROM irrigadores;";
 
-    // Estacoes
-    try (
-        Connection conn = new ConexaoMysql().getConnection();
-        PreparedStatement sttm = conn.prepareStatement(sqlEstacoes);
-        ResultSet result = sttm.executeQuery()) {
+    try (Connection conn = new ConexaoMysql().getConnection()) {
+      // Try da Estaçâo
+      try (
+          PreparedStatement sttm = conn.prepareStatement(sqlEstacoes);
+          ResultSet result = sttm.executeQuery()) {
 
-      while (result.next()) {
-        Map<String, Object> item = new HashMap<>();
-        item.put("id", result.getString("id"));
-        item.put("nome", result.getString("nome"));
-        Timestamp ts = result.getTimestamp("data_hora");
-        String data_hora = ts == null ? null
-            : ts.toInstant().atZone(zonaBr).format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"));
-        item.put("data_hora", data_hora);
-        estacoes.add(item);
+        while (result.next()) {
+          Map<String, Object> item = new HashMap<>();
+          item.put("id", result.getString("id"));
+          item.put("nome", result.getString("nome"));
+          Timestamp ts = result.getTimestamp("data_hora");
+          String data_hora = ts == null ? null
+              : ts.toInstant().atZone(zonaBr).format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"));
+          item.put("data_hora", data_hora);
+          estacoes.add(item);
+        }
+
+      } catch (Exception e) {
+        throw new SQLException("Erro ao buscar estacoes: " + e.getMessage());
+      }
+
+      // Try do Irrigador
+      try (
+          PreparedStatement sttm = conn.prepareStatement(sqlIrrigadores);
+          ResultSet result = sttm.executeQuery()) {
+
+        while (result.next()) {
+          Map<String, Object> item = new HashMap<>();
+          item.put("id", result.getInt("id"));
+          item.put("plantio", result.getString("plantio"));
+          item.put("acao_atual", result.getString("acao_atual"));
+          item.put("id_estacao", result.getString("id_estacao"));
+          irrigadores.add(item);
+        }
+      } catch (Exception e) {
+        throw new SQLException("Erro ao buscar irrigadores: " + e.getMessage());
       }
 
     } catch (Exception e) {
-      throw new SQLException("Erro ao buscar estacoes: " + e.getMessage());
-    }
-
-    // ------ IRRIGADORES ------
-    try (
-        Connection conn = new ConexaoMysql().getConnection();
-        PreparedStatement sttm = conn.prepareStatement(sqlIrrigadores);
-        ResultSet result = sttm.executeQuery()) {
-
-      while (result.next()) {
-        Map<String, Object> item = new HashMap<>();
-        item.put("id", result.getInt("id"));
-        item.put("plantio", result.getString("plantio"));
-        item.put("acao_atual", result.getString("acao_atual"));
-        item.put("id_estacao", result.getString("id_estacao"));
-        irrigadores.add(item);
-      }
-
-    } catch (Exception e) {
-      throw new SQLException("Erro ao buscar irrigadores: " + e.getMessage());
+      throw new SQLException("Erro ao conectar ao BD" + e.getMessage());
     }
 
     // JSON FINAL COM OS DOIS
